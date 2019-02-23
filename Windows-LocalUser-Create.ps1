0de8f5c0b50064665b6856b06064b36eb219dd63
21a1ss3 <21a1ss3@gmail.com>
2019-02-23 16:36:06 +0800

    Initial publish for windows local accounts scripts

---
+++ Windows-LocalUser-Create.ps1
@@ -0,0 +1,75 @@
+﻿param(  [Byte]$usrCount=0,
+        [Byte]$pwdType=255,
+        [string]$pwdTemplate=$null)
+
+
+
+if($usrCount -eq 0)
+{
+    do
+    {
+        Write-Host 
+        $usrCountStr = Read-Host -Prompt "Enter users count"
+    }
+    while (-Not [System.Byte]::TryParse($usrCountStr, [ref] $usrCount))
+}
+
+while(-Not (($pwdType -eq 1) -or ($pwdType -eq 2)))
+{
+    do
+    {    
+        Write-Host 
+        Write-Host "Select password source type: "
+        Write-Host "`t1. Use password template;"
+        Write-Host "`t2. Use console input;"
+
+        $pwdTypeStr = Read-Host -Prompt "Enter type"
+    }
+    while (-Not [System.Byte]::TryParse($pwdTypeStr, [ref] $pwdType))
+}
+
+
+if ($pwdType -eq 1)
+{
+    while ([System.String]::IsNullOrWhiteSpace($pwdTemplate))
+    {
+        $pwdTemplate = Read-Host -Prompt "Specify template string (parts: {0} - user index)"
+    }
+}
+elseif ($pwdType -eq 2)
+{
+    $pwdList = New-Object -TypeName 'System.Collections.Generic.List[System.String]' -ArgumentList @($usrCount)
+
+    for($i=1; $i -le $usrCount; $i++)
+    {
+        $cPwd = $null
+        do
+        {
+            $cPwd = Read-Host -Prompt "Password [$i]"
+        } while ([System.String]::IsNullOrWhiteSpace($cPwd))
+
+        $pwdList.Add($cPwd)
+    }
+}
+
+
+for ($i=1; $i -le $usrCount; $i++)
+{
+    if ($pwdType -eq 1)
+    {
+        $pwd = [System.String]::Format($pwdTemplate, $i);
+    }
+    elseif ($pwdType -eq 2)
+    {
+        $pwd = $pwdList[$i-1];
+    }
+
+    $secPwd = New-Object -TypeName System.Security.SecureString
+
+    foreach ($pwdChar in $pwd.ToCharArray())
+    {
+        $secPwd.AppendChar($pwdChar);
+    }
+
+    New-LocalUser -Name "user$i" -Password $secPwd 
+}
---
+++ Windows-LocalUser-Remove.ps1
@@ -0,0 +1,18 @@
+﻿param([Byte]$usrCount=0)
+
+
+
+if($usrCount -eq 0)
+{
+    do
+    {
+        Write-Host 
+        $usrCountStr = Read-Host -Prompt "Enter users count"
+    }
+    while (-Not [System.Byte]::TryParse($usrCountStr, [ref] $usrCount))
+}
+
+for ($i=1; $i -le $usrCount; $i++)
+{
+    Remove-LocalUser -Name "user$i"
+}
